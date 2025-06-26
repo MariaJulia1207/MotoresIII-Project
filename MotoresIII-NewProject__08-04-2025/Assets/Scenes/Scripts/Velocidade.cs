486c10a54aa1379bc825a52729644b4beec2fede
MariaJulia1207 <maria.palhares@escolar.ifrn.edu.br>
2025-06-26 10:15:28 -0300

    26/06/2025 - 10:15

--- MotoresIII-NewProject__08-04-2025/Assets/Scenes/Scripts/Velocidade.cs
+++ MotoresIII-NewProject__08-04-2025/Assets/Scenes/Scripts/Velocidade.cs
@@ -4,35 +4,20 @@ public class Velocidade : MonoBehaviour
 {
     public float velocidade = 5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private Rigidbody2D _rigidbody2D;
-    private SpriteRenderer spriteRenderer;
-
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
-            //rigidbody2D.AddForce(new Vector2(-velocidade,0));
-            spriteRenderer.flipX = false;
-            Debug.Log("LeftArrow");
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
-            //rigidbody2D.AddForce(new Vector2(velocidade,0));
-            spriteRenderer.flipX = true;
-            Debug.Log("RightArrow");
-        }
+        float movimentoHorizontal = 0f;
+        if (Input.GetKey(KeyCode.LeftArrow)) { movimentoHorizontal = -1f; }
+        else if (Input.GetKey(KeyCode.RightArrow)) { movimentoHorizontal = 1f; }
+        else { movimentoHorizontal = 0f; }
+        transform.position += new Vector3(movimentoHorizontal, 0f, 0f) * velocidade * Time.deltaTime;
 
         float movimentoVertical = 0f;
         if (Input.GetKey(KeyCode.UpArrow)) { movimentoVertical = 1f; }
